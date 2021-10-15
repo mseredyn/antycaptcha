60beaf09156b355165767899a605a406aed76261
mseredyn <michalseredyn@gmail.com>
2021-10-15 20:47:16 +0200

    added page_objects

--- page_object/exercise4_page.py
+++ page_object/exercise4_page.py
@@ -6,5 +6,5 @@ class Exercise4Page(CommonPage):
     def __init__(self, driver):
         super().__init__(driver)
 
-    def click_input_by_its_group_and_value_param(self, group, value_param):
-        self.click_by_xpath(f'//h5[text()="{group}"]/../input[@value="{value_param}"]')
+    def click_input_by_its_group_and_value(self, group, value):
+        self.click_by_xpath(f'//h5[text()="{group}"]/../input[@value="{value}"]')
