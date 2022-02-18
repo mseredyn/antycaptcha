22bd10d09db4db66f52337b5173ac982876aa030
mseredyn <7304563+mseredyn@users.noreply.github.com>
2022-02-18 08:52:42 +0100

    Move to playwright (#3)
    * added page_objects
    
    * added tests, refactored page files
    
    * removed selenium, added playwright
    
    * added page_objects
    
    * added tests, refactored page files
    
    * removed selenium, added playwright

--- page_object/base_page.py
+++ page_object/base_page.py
@@ -1,22 +1,28 @@
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.wait import WebDriverWait
 
+import logging
 
+LOGGER = logging.getLogger(__name__)
 class BasePage:
 
-    def __init__(self, driver):
-        self.driver = driver
+    def __init__(self, page):
+        self.page = page
 
     def find_by_xpath(self, xpath):
-        WebDriverWait(self.driver, 15).until(EC.presence_of_element_located((By.XPATH, xpath)))
-        return self.driver.find_element_by_xpath(xpath)
+        LOGGER.info(f'Looking for element by xpath: {xpath}')
+        element = self.page.locator(f"xpath={xpath}")
+        LOGGER.info(f'Found element: {element}')
+        return element
 
     def click_by_xpath(self, xpath):
+        LOGGER.info(f'Clicking element by xpath: {xpath}')
         self.find_by_xpath(xpath).click()
 
     def fill_with_value_by_xpath(self, xpath, value_to_fill):
-        self.find_by_xpath(xpath).send_keys(value_to_fill)
+        LOGGER.info(f'Filling element of xpath: {xpath} with value: {value_to_fill}')
+        self.find_by_xpath(xpath).fill(value_to_fill)
 
     def get_text_of_element_by_xpath(self, xpath):
-        return self.find_by_xpath(xpath).text
+        LOGGER.info(f'Fetching text of element by xpath: {xpath}')
+        text = self.find_by_xpath(xpath).text_content()
+        LOGGER.info(f'Found text: {text}')
+        return text
